20010f0ea3e710c3999087fd260cb1778c4b1b1c
Nadav Sinai <87267595+nadavsinai-philips@users.noreply.github.com>
2023-12-28 23:00:51 +0000

    fix: Support false inputs better

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import type { pullRequest } from './interfaces';
 async function run(): Promise<void> {
 	const pullRequestPayload = github.context.payload.pull_request;
 	const configPath = core.getInput('cl-config');
-	const useDescription = !!core.getInput('useDescription');
-	const convertToCJS = !!core.getInput('convertToCJS');
+	const useDescription = core.getBooleanInput('useDescription') ?? false;
+	const convertToCJS = core.getBooleanInput('convertToCJS') ?? false;
 
 	if (!pullRequestPayload?.title)
 		throw new Error('Pull Request or Title not found!');
